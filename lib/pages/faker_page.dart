cd14ac894c32c5d6dab2e2f0a90fe511329fc368
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-02-14 02:06:24 +0700

    add intl package

--- lib/pages/faker_page.dart
+++ lib/pages/faker_page.dart
@@ -2,8 +2,10 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:faker/faker.dart';
+import 'package:intl/intl.dart';
 
 class FakerPage extends StatelessWidget {
+  FakerPage({Key? key}) : super(key: key);
   var faker = Faker();
 
   @override
@@ -14,18 +16,46 @@ class FakerPage extends StatelessWidget {
           "FAKER",
         ),
       ),
-      body: ListView.builder(
-        itemCount: 15,
-        itemBuilder: (context, index) => ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Colors.grey[300],
-            backgroundImage: NetworkImage(
-              "https://picsum.photos/id/${151 + index}/200/300",
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 10),
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            height: 50,
+            width: 200,
+            child: Center(
+              // Format waktu menggunakan package intl
+              child: Text(
+                DateFormat.yMMMMd().format(DateTime.now()),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
             ),
           ),
-          title: Text(faker.person.name()),
-          subtitle: Text(faker.internet.email()),
-        ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 15,
+              itemBuilder: (context, index) => ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.grey[300],
+                  backgroundImage: NetworkImage(
+                    "https://picsum.photos/id/${151 + index}/200/300",
+                  ),
+                ),
+                // Data dengan faker
+                title: Text(faker.person.name()),
+                subtitle: Text(
+                  faker.internet.email(),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
