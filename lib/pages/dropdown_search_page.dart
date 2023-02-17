423c64eb574c92634b9322ed761e11be78c9f66c
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-02-17 23:07:09 +0700

    add dropdown API

--- lib/pages/dropdown_search_page.dart
+++ lib/pages/dropdown_search_page.dart
@@ -1,51 +1,79 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter_package_test/models/province_models.dart';
+import 'package:http/http.dart' as http;
 
 class DropDownSearchPage extends StatelessWidget {
-  List<Map<String, dynamic>> data = [
-    {
-      "country": "Brazil",
-      "id": 1,
-    },
-    {
-      "country": "Italy",
-      "id": 2,
-    },
-    {
-      "country": "Indonesia",
-      "id": 3,
-    },
-    {
-      "country": "Canada",
-      "id": 1,
-    },
-  ];
+  List<Map<String, dynamic>> allProvince = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text("Dropdown Search"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: DropdownSearch<Map<String, dynamic>>(
-          validator: (value) {
-            if (value == null) {
-              return 'required filed';
-            } else {
-              return value['country'];
-            }
-          },
-          onChanged: (value) => print(value),
-          popupProps: const PopupProps.menu(
-            fit: FlexFit.tight,
-            showSearchBox: true,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: DropdownSearch<Map<String, dynamic>>(
+              asyncItems: (text) async {
+                var response = await http.get(
+                  Uri.parse(
+                      "http://www.emsifa.com/api-wilayah-indonesia/api/provinces.json"),
+                );
+                if (response.statusCode == 200) {
+                  List data = json.decode(response.body) as List;
+                  data.forEach((element) {
+                    allProvince.add(element);
+                  });
+                  return allProvince;
+                } else {
+                  return [];
+                }
+              },
+              dropdownBuilder: (context, selectedItem) => Text(
+                selectedItem?["name"] ?? "Pilih data",
+              ),
+              validator: (value) {
+                if (value == null) {
+                  return 'required filed';
+                } else {
+                  return value['name'];
+                }
+              },
+              onChanged: (value) => print(value),
+              popupProps: PopupProps.menu(
+                fit: FlexFit.tight,
+                showSearchBox: true,
+                itemBuilder: (context, item, isSelected) {
+                  return ListTile(
+                    title: Text(item['name'].toString()),
+                  );
+                },
+              ),
+              showClearButton: true,
+              // selectedItem: allProvince[],
+              items: allProvince,
+            ),
           ),
-          showClearButton: true,
-          selectedItem: data[0],
-          items: data,
-        ),
+          ElevatedButton(
+            onPressed: () async {
+              var response = await http.get(
+                Uri.parse(
+                    "http://www.emsifa.com/api-wilayah-indonesia/api/provinces.json"),
+              );
+              // print(response.body);
+              List data = json.decode(response.body) as List;
+
+              print(data[1]["name"]);
+            },
+            child: const Text("Get Data"),
+          )
+        ],
       ),
     );
   }
